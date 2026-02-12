24c40b0c18103252928609fa69138740a66e8595
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2026-02-12 16:52:09 +0400

    dfsd

--- src/app/[locale]/ticket/components/TicketsHeader.jsx
+++ src/app/[locale]/ticket/components/TicketsHeader.jsx
@@ -7,12 +7,9 @@ import TicketsInfoCards from "./TicketsInfoCards";
 export default function TicketsHeader() {
   return (
     <header className=" min-h-[420px] md:min-h-[520px] overflow-hidden">
-      {/* Blurred background - stage/microphone feel */}
-    
 
-      <div className="relative container mx-auto z-10  grid grid-cols-1 md:grid-cols-3 gap-10 pt-20 md:pt-28 pb-12 md:pb-16 text-center px-4">
-        <div className="text-left md:col-span-2">
-          <h1
+      <div className="container mx-auto relative z-10 flex flex-col items-center justify-center h-full pt-10 md:pt-14">
+     <h1
             className="font-serif text-[42px] md:text-[48px] lg:text-[56px] font-semibold tracking-wide"
             style={{
               background: "linear-gradient(180deg, #F0E68C 0%, #E9DDCF 100%)",
@@ -27,6 +24,13 @@ export default function TicketsHeader() {
           <p className="mt-2 md:mt-2 text-base md:text-[18px] text-[#F5F5F5] font-normal leading-relaxed">
             Apply for press access and official media coverage opportunities with GTCFX.
           </p>
+      </div>
+      {/* Blurred background - stage/microphone feel */}
+    
+
+      <div className="relative container mx-auto z-10  grid grid-cols-1 md:grid-cols-3 pt-10 text-center">
+        <div className="text-left md:col-span-2">
+     
           <TicketsInfoCards/>
         </div>
         <NewFormDesign />
